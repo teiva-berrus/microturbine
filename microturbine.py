fdb90016853bc8062edf721433eeb67af95277ae
teiva-berrus <teiva.berrus@gmail.com>
2022-11-16 16:49:31 +0100

    fix phi ratio problem

--- microturbine.py
+++ microturbine.py
@@ -9,8 +9,9 @@ INTAKE_TEMP = 293           # in Kelvin
 INTAKE_PRES = 1E5           # in Pa
 AIR_MASS_FLOW = 20          # in g/s
 HYDROGEN_MASS_FLOW = 0.097  #in g/s
-DILUTION_RATIO = AIR_MASS_FLOW/HYDROGEN_MASS_FLOW
-RICHESSE = 34/DILUTION_RATIO
+RICHESSE = (1/0.42)/((AIR_MASS_FLOW/29)/(HYDROGEN_MASS_FLOW/2))
+
+print("Richesse : " + str(round(RICHESSE,2)))
 GAMMA = 1.4
 gas = ct.Solution('gri30.yaml')
 gas.TPX = INTAKE_TEMP, INTAKE_PRES, 'N2:{0}, O2:{1}'.format(0.8,0.2)
@@ -55,7 +56,7 @@ gas.TP = T3,P3
 
 """ Combustion chamber"""
 
-gas.set_equivalence_ratio(phi = RICHESSE, fuel = 'H2', oxidizer = 'O2: 0.2, N2: 0.8')
+gas.set_equivalence_ratio(phi = RICHESSE, fuel = 'H2', oxidizer = 'O2: 0.21, N2: 0.79')
 gas.equilibrate('HP')
 
 T4 = gas.T
@@ -67,8 +68,6 @@ enthalpy.append(H4)
 entropy.append(gas.s)
 
 print("Flamme temperature = " + str(int(T4)) + " K")
-
-
 
 
 """ Turbine """
@@ -93,11 +92,12 @@ TURBINE_POWER_ISENTROPIC = (H4-H5)*AIR_MASS_FLOW*1E-3
 TURBINE_POWER = TURBINE_POWER_ISENTROPIC*0.73
 T5 = T4 + TURBINE_POWER/(gas.cp*AIR_MASS_FLOW*1E-3)
 print("Turbine power (isentropic) = " + str(int(TURBINE_POWER_ISENTROPIC)) + " W")
-print("Turbine power = " + str(int(TURBINE_POWER_ISENTROPIC)) + " W")
+print("Turbine power = " + str(int(TURBINE_POWER)) + " W")
 print("Available power = " + str(int(TURBINE_POWER-COMPRESSION_POWER)) + " W")
-plt.show()
+#plt.show()
+print(T5)
 
+# Rendement
 
-
-
-
+EFFICENCY = (TURBINE_POWER-COMPRESSION_POWER)/(143E6*HYDROGEN_MASS_FLOW*1E-3)
+print("Rendement = " + str(EFFICENCY*100) + " %")
